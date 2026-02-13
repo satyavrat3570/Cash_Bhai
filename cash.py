ff1d1cb35247b29e91c15add1fa1be52e5837ae4
Satyavrat Govind <satyavrat3570@gmail.com>
2026-02-13 19:11:56 +0530

    Replace cash message with a new print statement

--- cash.py
+++ cash.py
@@ -1,9 +1,1 @@
-print("Cash Hai to Aish Hai")
-question_solved= input("Cash hai to kya sach mein aish hai?")
-if question_solved==True:
-  bomb_detonate=True
-  break
-elif question_solved==False:
-  bomb_detonate=True
-  break
-print("Matlab janaab dono cases mein boom pakka hai, Hahaha!!!") 
+print("Vaishnav Jan to tene kahiye je, peed parai jaane re!")
