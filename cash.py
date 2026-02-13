00062e2f70f500b15cb35c4823eda38c234a2e73
Satyavrat Govind <satyavrat3570@gmail.com>
2026-02-13 19:07:56 +0530

    Implement user input for cash-related question
    Added user input handling for a question about cash.

--- cash.py
+++ cash.py
@@ -1,1 +1,9 @@
 print("Cash Hai to Aish Hai")
+question_solved= input("Cash hai to kya sach mein aish hai?")
+if question_solved==True:
+  bomb_detonate=True
+  break
+elif question_solved==False:
+  bomb_detonate=True
+  break
+print("Matlab janaab dono cases mein boom pakka hai, Hahaha!!!") 
